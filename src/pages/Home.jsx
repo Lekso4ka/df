0b6c5dfbc5e1f73e42c8c70514534e83f2ba2291
Leksa <admin@petiteweb.ru>
2023-05-22 06:09:38 +0300

    Создание компонента Layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
+import Layout from "../components/Layout";
+
 export function Home () {
     return <>
         <h1>Главная страница</h1>
+        <Layout mb={1} dt={2} gap="small" />
+        <Layout mb={2} dt={4} gap="none" />
+        <Layout mb={2} dt={3} />
     </>
 }
