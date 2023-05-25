59c1978acbea6699b9b846ce45572fc90915dd7f
Leksa <admin@petiteweb.ru>
2023-05-25 13:50:23 +0300

    Добавлены картинки и текст для рекламы

---
+++ src/assets/images/corgi/index.js
@@ -0,0 +1,25 @@
+import pic1 from "./corgi1.png"
+import pic2 from "./corgi2.png"
+import pic3 from "./corgi3.png"
+import pic4 from "./corgi4.png"
+import pic5 from "./corgi5.png"
+import pic6 from "./corgi6.png"
+import pic7 from "./corgi7.png"
+import pic8 from "./corgi8.png"
+import pic9 from "./corgi9.png"
+import pic10 from "./corgi10.png"
+
+const Corgi = {
+    toys_ball: pic1,
+    delicious_pack: pic2,
+    delicious_feed: pic3,
+    toys_ball2: pic4,
+    outerwear_sock: pic5,
+    box: pic6,
+    logo: pic7,
+    delicious_bone: pic8,
+    corgi: pic9,
+    delicious_candy: pic10
+}
+
+export default Corgi
