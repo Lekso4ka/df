56b99392dc236a7b675e56d2f845dbcc52ae616b
Leksa <admin@petiteweb.ru>
2023-06-16 03:58:06 +0300

    hotfix

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,15 +8,15 @@ import "./index.css";
 const Footer = () => {
     return <footer className="footer">
         <Layout dt={3}>
+            <div className="footer__menu">
+                <Nav menu={menu.footer} position="vertical"/>
+            </div>
             <div className="footer__sign">
                 <Logo position="vertical"/>
                 <div className="footer__text">
                     <span>Make by <a href="https://github.com/lekso4ka" target="_blank" rel="noreferrer">Leksa</a> width <i className="lni lni-heart"/></span>
                     <span>©{new Date().getFullYear()} All rights reserved</span>
                 </div>
-            </div>
-            <div className="footer__menu">
-                <Nav menu={menu.footer} position="vertical"/>
             </div>
             <hr className="footer__separator"/>
             <Nav menu={menu.links_1} position="vertical"/>
